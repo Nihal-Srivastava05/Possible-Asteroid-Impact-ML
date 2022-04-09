31c9f4e8ae82d23e61f559be1fbd0594c78c72b7
Nihal Srivastava <82692408+Nihal-Srivastava05@users.noreply.github.com>
2022-04-09 11:31:52 +0530

    Loaded and Predicted using the model.

--- deployment/app.py
+++ deployment/app.py
@@ -12,54 +12,82 @@ def home_page():
 @app.route('/classification', methods=['GET', 'POST'])
 def classification_page():
     if request.method == 'POST':
-        epoch = request.form.get('epoch')
-        axis = request.form.get('axis')
-        eccentricity = request.form.get('eccentricity')
-        inclination = request.form.get('inclination')
-        argument = request.form.get('argument')
-        longitude = request.form.get('longitude')
-        anomoly = request.form.get('anomoly')
-        perihelion = request.form.get('perihelion')
-        aphelion = request.form.get('aphelion')
-        period = request.form.get('period')
-        intersection = request.form.get('intersection')
-        reference = request.form.get('reference')
-        magnitude = request.form.get('magnitude')
+        epoch = float(request.form.get('epoch'))
+        axis = float(request.form.get('axis'))
+        eccentricity = float(request.form.get('eccentricity'))
+        inclination = float(request.form.get('inclination'))
+        argument = float(request.form.get('argument'))
+        longitude = float(request.form.get('longitude'))
+        anomoly = float(request.form.get('anomoly'))
+        perihelion = float(request.form.get('perihelion'))
+        aphelion = float(request.form.get('aphelion'))
+        period = float(request.form.get('period'))
+        intersection = float(request.form.get('intersection'))
+        reference = float(request.form.get('reference'))
+        magnitude = float(request.form.get('magnitude'))
         data = [epoch, axis, eccentricity, inclination, argument, longitude, anomoly, perihelion, aphelion, period, intersection, reference, magnitude]
-        
+        model_file = '../models/AsteroidClassification.pkl'
+        classification_model = pickle.load(open(model_file, 'rb'))
+
+        prediction = classification_model.predict([data])
+        object_mapping = ['Amor Asteroid', 'Amor Asteroid (Hazard)', 'Apohele Asteroid',
+                          'Apohele Asteroid (Hazard)', 'Apollo Asteroid',
+                          'Apollo Asteroid (Hazard)', 'Aten Asteroid',
+                          'Aten Asteroid (Hazard)']
+
+        print(object_mapping[int(prediction[0])])
 
     return render_template('classification.html')
 
 @app.route('/impacts', methods=['GET', 'POST'])
 def impacts_page():
     if request.method == 'POST':
-        start = request.form.get('start')
-        end = request.form.get('end')
-        probability = request.form.get('probability')
-        velocity = request.form.get('velocity')
-        magnitude = request.form.get('magnitude')
-        diameter = request.form.get('diameter')
-        cpalermo = request.form.get('cpalermo')
-        mpalermo = request.form.get('mpalermo')
-        torino = request.form.get('torino')
+        start = float(request.form.get('start'))
+        end = float(request.form.get('end'))
+        probability = float(request.form.get('probability'))
+        velocity = float(request.form.get('velocity'))
+        magnitude = float(request.form.get('magnitude'))
+        diameter = float(request.form.get('diameter'))
+        cpalermo = float(request.form.get('cpalermo'))
+        mpalermo = float(request.form.get('mpalermo'))
+        torino = float(request.form.get('torino'))
         data = [start, end, probability, velocity, magnitude, diameter, cpalermo, mpalermo, torino]
+        model_file = '../models/AsteroidImpactsElasticNet.pkl'
+        impact_model = pickle.load(open(model_file, 'rb'))
+
+        polynominal_converter = pickle.load(open('../models/polynominal_converter.pkl', 'rb'))
+        data = polynominal_converter.transform([np.array(data)])
+        best_features = [ 0,  9, 36, 15, 48,  6, 23, 37, 12,  3, 20,  1, 34, 49, 41, 18, 33, 21, 10, 39, 16,  4, 51, 45,  7, 46, 35, 24, 40]
+        data = data[0][best_features]
+        scaler = pickle.load(open('../models/AsteroidImpactsScaler.pkl', 'rb'))
+        X = scaler.transform([data])
+
+        prediction = impact_model.predict(X)
+        print(prediction)
 
     return render_template('impacts.html')
 
 @app.route('/clustering', methods=['GET', 'POST'])
 def clustering_page():
     if request.method == 'POST':
-        start = request.form.get('start')
-        end = request.form.get('end')
-        impacts = request.form.get('impacts')
-        probability = request.form.get('probability')
-        velocity = request.form.get('velocity')
-        magnitude = request.form.get('magnitude')
-        diameter = request.form.get('diameter')
-        cpalermo = request.form.get('cpalermo')
-        mpalermo = request.form.get('mpalermo')
-        torino = request.form.get('torino')
+        start = float(request.form.get('start'))
+        end = float(request.form.get('end'))
+        impacts = float(request.form.get('impacts'))
+        probability = float(request.form.get('probability'))
+        velocity = float(request.form.get('velocity'))
+        magnitude = float(request.form.get('magnitude'))
+        diameter = float(request.form.get('diameter'))
+        cpalermo = float(request.form.get('cpalermo'))
+        mpalermo = float(request.form.get('mpalermo'))
+        torino = float(request.form.get('torino'))
         data = [start, end, impacts, probability, velocity, magnitude, diameter, cpalermo, mpalermo, torino]
+        model_file = '../models/AsteroidClustering.pkl'
+        clustering_model = pickle.load(open(model_file, 'rb'))
+
+        cluster_impact_mapping = {2:5, 5:4, 0:3, 4:2, 3:1, 1:0}
+        prediction = clustering_model.predict([data])
+        cluster_impact = cluster_impact_mapping[prediction[0]]
+        print(cluster_impact)
 
     return render_template('clustering.html')
 
